67120c26c399d6ab64729f2414dfc800803dbf53
DJStefan <stefan.kussmaul@gmail.com>
2018-03-21 19:51:39 -0400

    Added Initial Models for Comment and Notification

--- project2/gitpushforce/umoc/admin.py
+++ project2/gitpushforce/umoc/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Trip
+from .models import User, Trip, Comment, Notification
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Trip) 
+admin.site.register(Comment)
+admin.site.register(Notification)
--- project2/gitpushforce/umoc/models.py
+++ project2/gitpushforce/umoc/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 # Create your models here.
 
 
-class User(models.Model):
+class User(models.Model): # TODO: CAN_COMMENT, CAN_JOIN_TRIP
 	"""
 	Model representing a user of the website (UMOC club member). Stores all 
 	personal data.
@@ -33,7 +33,6 @@ class User(models.Model):
 		
 
 class Trip(models.Model):
-
 	""" 
 	Represents a scheduled trip.
 	"""
@@ -70,3 +69,35 @@ class Trip(models.Model):
     
 	def __str__(self):
 		return 'Trip "{}", running from {} to {}'.format(self.name, self.start_time, self.end_time)
+
+		
+class Comment(models.Model):
+	""" 
+	Represents a comment left by a user on a trip. Has an author (User), a timestamp, a parent comment if it is a reply (can be None), and the Trip it is commenting on.
+	"""
+	author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+	parent = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
+	text = models.CharField(default='', max_length=280)
+	time_stamp = models.DateTimeField() # TODO: DEFAULT TO NOW
+	trip = models.ForeignKey(Trip, on_delete=models.SET_NULL, null=True)
+	
+	#class Meta:
+	#	ordering = ['trip.id']
+
+	# returns url to trip
+	def get_absolute_url(self):
+		return reverse('trip_info', args=[str(self.trip.id)])
+    
+	def __str__(self):
+		return 'Comment by {} on trip {}. Replying to {} on {}'.format(self.author.first_name, self.trip.name, self.parent.author.first_name if self.parent else '', self.time_stamp)
+		
+
+class Notification(models.Model):
+	""" 
+	A Notification is sent to a user when something relevant to them occurs. Examples are: one of their comments gets a reply, one of their planned trips is canceled, their trip is coming up soon. A Notification simply stores a reference to the User, a message, a timestamp of when it was sent, and a bool (whether it has been seen yet). It can also include an optional url, which when clicked will direct the user to a relevant page.
+	"""
+	recipient = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+	message = models.TextField()
+	seen = models.BooleanField(default=False)
+	link = models.URLField(max_length=100, blank=True)
+	time_stamp = models.DateTimeField()
