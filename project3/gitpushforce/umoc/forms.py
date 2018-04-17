d1333b5ed7ea4af835af47533f4aa8c3b0f86182
Nonu Bajaj <35976755+nihitbajaj@users.noreply.github.com>
2018-04-16 22:25:58 -0400

    Completed login and registration pages

--- project3/gitpushforce/umoc/forms.py
+++ project3/gitpushforce/umoc/forms.py
@@ -7,11 +7,10 @@ class RegisterForm(UserCreationForm):
 	first_name = forms.CharField(max_length=30, help_text='Required - Please enter your first name.')
 	last_name = forms.CharField(max_length=30, help_text='Required - Please enter your last name.')
 	email = forms.EmailField(max_length=150, help_text='Required. Please enter your email address.')
-	email_confirm = forms.EmailField(max_length=150, help_text='Required. Please confirm your email address.')
 
 	class Meta:
 		model = User
-		fields = ('username', 'first_name', 'last_name', 'email', 'email_confirm', 'password1', 'password2')
+		fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
 
 
 class AdminTripForm(forms.Form):
