8198e7c44010253408dffee2ea51b8f7cbd50998
DJStefan <stefan.kussmaul@gmail.com>
2018-04-21 00:05:45 -0400

    Rewrote comments.js to Use jQuery

--- project4/gitpushforce/umoc/static/javascript/comments.js
+++ project4/gitpushforce/umoc/static/javascript/comments.js
@@ -34,6 +34,7 @@ $.ajaxSetup({
 });
 
 $(document).ready(function(){
+	// query trip comments and append HTML
 	$.ajax({
 		type: "get",
 		url: "http://localhost:8000/trip/" + trip_id + "/comments",
@@ -41,156 +42,56 @@ $(document).ready(function(){
 		success: function(data) {
 			console.log('Received response');
 			console.log(data);
-			console.log(getCookie('csrftoken'));
 			
-			// mapping of comment ids to comment objects
-			var comments = new Map();
+			// append rendered comment section below comments-header element
+			$('#comments-header').append(data);
 			
-			for (var i = 0; i < data.length; i++) {
-				// list of ids of replies to this comment
-				data[i].replies = []
+			// create click handlers for each reply button
+			$('.comment-reply-btn').on('click', function() {
+				// extract comment id ("reply-btn-<id>")
+				var id = $(this).attr('id').substring(10);
+				console.log('clicked ' + id);
 				
-				comments.set(data[i].id, data[i]);
-				console.log(comments.size);
-				console.log(comments.get(data[i].id));
-			}
+				// remove reply button from parent comment
+				$(this).remove();
+				
+				// create input element for reply
+				var reply_input = $('<input/>', {
+					id: 'reply-input-' + id
+				});
 			
-			// list of comment ids that are base comments
-			base_comments = []
-			
-			// build parent associations
-			for (const id of comments) {
-
-				if (comments.get(id[0]).parent === 0) {
-					base_comments.push(id[0]);
-				} else {
-					comments.get(comments.get(id[0]).parent).replies.push(id[0]);
-				}
-			}
-
-			var comment_header = document.getElementById('comments-header');
-			
-			// render comment threads
-			for (var i = 0; i < base_comments.length; i++) {
-				var rendered_dom = renderThread(comments, base_comments[i], 0);
-				comment_header.append(rendered_dom); // TODO: this will add them in reverse order of creation
-			}
+				// create button to submit reply
+				var reply_btn = $('<button/>', {
+					text: 'Reply', //set text 1 to 10
+					id: 'submit-btn-' + id,
+					click: function () { 
+						console.log('Clicked submit on ' + id);
+						// submit AJAX post with reply data
+						$.ajax({
+							type: "POST",
+							dataType: "application/json",
+							url: "http://localhost:8000/trip/" + trip_id + "/comments",
+							data: {'text': reply_input.val(), 'parent': id},
+							success: function(result) {
+								console.log(result);
+								
+							},
+							error: function(result) {
+								console.log('Error with POST');
+								console.log(result)
+							}
+						})
+					}
+				});
+				
+				// add input element and button after comment's div
+				$('#comment-' + id).after(reply_input);
+				$('#reply-input-' + id).after(reply_btn);
+				
+			});
 		},
 		error: function(){
 			console.log('AJAX error');
 		}
 	});
 });
-
-// renders comment thread starting with given id. comments is a mapping of ids to comment objects. Depth is the depth of the current thread. Returns rendered DOM element
-function renderThread(comments, id, depth) {
-	console.log('Rendering');
-	var div = document.createElement('div');
-	div.setAttribute('id', 'comment-' + id);
-	div.style.paddingLeft = 30 * depth + 'px';
-	div.style.paddingBottom = 10;
-	
-	div.className = 'comment';
-	div.style.backgroundColor = '#d3d3d3';
-	
-	div.innerHTML = '<p>' + comments.get(id).author_name + (comments.get(id).parent !== 0 ? ' Replying to ' + comments.get(comments.get(id).parent).author_name : '') + ' on ' + comments.get(id).timestamp  + '</p>';
-	
-	div.innerHTML += '<p>' + comments.get(id).text + '</p>';
-	
-	var reply_btn = document.createElement('buton');
-	reply_btn.setAttribute('id', 'reply-btn-' + id);
-	reply_btn.className = 'btn btn-primary';
-	reply_btn.innerHTML = 'Reply';
-	reply_btn.onclick = function() {
-		console.log('Clicked button' + id);
-		reply_btn.remove();
-		
-		var reply_form = document.createElement('div');
-		reply_form.setAttribute('id', 'reply-form-' + id);
-		var comment_input = document.createElement('input');
-		reply_form.append(comment_input);
-		// reply_form.innerHTML = '<input></input>';
-		
-		var submit_btn = document.createElement('button');
-		submit_btn.className = 'btn btn-primary';
-		submit_btn.innerHTML = 'Reply';
-		submit_btn.onclick = function() {
-			console.log('Clicked button');
-			$.ajax({
-				type: "POST",
-				dataType: "application/json",
-				url: "http://localhost:8000/trip/" + trip_id + "/comments",
-				data: {'text': comment_input.value, 'parent': id},
-				success: function(result) {
-					console.log(result);
-				},
-				error: function(result) {
-					console.log('Error with POST');
-					console.log(result)
-				}
-			})
-		}
-		
-		reply_form.append(submit_btn);
-		
-		div.append(reply_form);
-	}
-	
-	div.append(reply_btn);
-	
-	for (var i = 0; i < comments.get(id).replies.length; i++) {
-		div.appendChild(renderThread(comments, comments.get(id).replies[i], depth + 1));
-	}
-	return div;
-}
-/*
-var comments = document.getElementsByClassName('comment');
-var reply_btns = document.getElementsByClassName('comment-reply-btn');
-
-console.log('Found ' + reply_btns.length + ' buttons');
-
-// note: I am not good at Javascript. Please feel free to revise (and better yet, tell me how I can make it better)
-for (var i = 0; i < reply_btns.length; i++) {
-	reply_btns[i].dataset.replied = 0;
-	handleElement(i);
-}
-	/*reply_btns[i].dataset.index = i;
-	console.log('Button created for comment ' + reply_btns[i].dataset.dataCommentId);
-
-	reply_btns[i].onclick = function(e) {
-		console.log('You clicked button for comment ' + e.target.dataset.index);
-		var reply = document.createElement('div');
-		reply.innerHTML = '<input type="text" placeholder="Comment" class="form-control" />';
-		e.target.appendChild(reply);
-	}
-	
-}
-
-function onClickReply(comment_index) {
-	console.log(comment_index);
-	
-}*/
-/*
-function handleElement(i) {
-    reply_btns[i].onclick=function() {
-		console.log('Comment ' + i + ' clicked. Replied = ' + reply_btns[i].dataset.replied);
-		// check comment has not been replied to
-		if (reply_btns[i].dataset.replied === '0') {
-			reply_btns[i].dataset.replied = '1';
-			var reply_div = document.createElement('div');
-			var text_input = document.createElement('input');
-			var send_btn = document.createElement('button');
-			
-			// have it send a post when clicked  TODO: USE DJANGO FORM
-			send_btn.onclick = function() {
-				console.log('User is sending ' + text_input.value);
-			}
-			
-			reply_div.appendChild(text_input);
-			reply_div.appendChild(send_btn);
-			//reply.innerHTML = '<input type="text" placeholder="Comment" class="form-control" /><button type="button">Send</button>';
-			
-			comments[i].appendChild(reply_div);
-		}
-    };
-}*/
